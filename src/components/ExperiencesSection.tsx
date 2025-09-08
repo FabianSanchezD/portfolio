41ae9944712647e1bb329786478b759275a8fc84
Fabian Sanchez <85092684+FabianSanchezD@users.noreply.github.com>
2025-09-08 09:15:57 -0600

    reworded

--- src/components/ExperiencesSection.tsx
+++ src/components/ExperiencesSection.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const experiences = [
     {
         "title": "Aleph Hackathon",
-        "description": "Winners of 2nd Place in the Aleph Hackathon (Filecoin Track). We built a DApp that added transparency to government tenders and bidding. All official documents were stored on-chain with Filecoin, and all processes were posible with Ethereum Smart Contracts.",
+        "description": "Winners of 2nd Place in the Aleph Hackathon (LATAM's biggest hackathon) in the Filecoin Track. We built a DApp that added transparency to government tenders and bidding. All processes were posible with Ethereum Smart Contracts and Filecoin.",
         "date": "August 2025",
         "link": "https://github.com/Althara-Labs/althara-dapp",
         "image": "/experiences/aleph-winners.jpeg",
