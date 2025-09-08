124d1d4efdb5e90cf3e3c02c2cd320608e49a813
Fabian Sanchez <85092684+FabianSanchezD@users.noreply.github.com>
2025-09-08 09:22:51 -0600

    added todos, changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,11 @@ const Navbar = () => {
   return (
     <header className="bg-gray-800 shadow-md rounded-2xl sticky">
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
-        <h1 className="text-xl font-bold text-white">Fabián Sánchez</h1>
+        {/* TODO: Add link to CV */}
+        <h1 className="text-xl font-bold text-white"><Link href="/#about">Fabián Sánchez</Link></h1>
         <ul className="flex gap-8 text-white font-medium pl-7">
-          <li><Link href="/#about" className="hover:text-gray-900 transition-colors">About</Link></li>
-          <li><Link href="/#projects" className="hover:text-gray-900 transition-colors">Projects</Link></li>
+            <li><Link href="/#projects" className="hover:text-gray-900 transition-colors">Projects</Link></li>
+            <li><Link href="/#experiences" className="hover:text-gray-900 transition-colors">Experiences</Link></li>
           <li><Link href="/#contact" className="hover:text-gray-900 transition-colors">Contact</Link></li>
         </ul>
       </nav>
