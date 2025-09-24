b0164890555573610949e920413d42e1cccb40ab
Fabian Sanchez <85092684+FabianSanchezD@users.noreply.github.com>
2025-09-24 09:37:07 -0600

    fix: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Link from "next/link";
+import { BrowserView, MobileView } from 'react-device-detect';
 
 const Navbar = () => {
   return (
     <header className="bg-gray-800 shadow-md rounded-2xl sticky">
-      <nav className="container mx-auto flex items-center justify-between py-4 px-6">
-        {/* TODO: Add link to CV */}
-        <h1 className="text-xl font-bold text-white"><Link href="/#about">Fabián Sánchez</Link></h1>
+      <nav className="container mx-auto flex items-center justify-between py-4 px-6"> 
+        <h1 className="text-xl font-bold text-white"><Link href="https://drive.google.com/file/d/1eEWExGSx8QNMXgu1gnkhn41AzjsbwBQH/view">Fabián Sánchez</Link></h1>
         <ul className="flex gap-8 text-white font-medium pl-7">
-            <li><Link href="/#projects" className="hover:text-gray-900 transition-colors">Projects</Link></li>
-            <li><Link href="/#experiences" className="hover:text-gray-900 transition-colors">Experiences</Link></li>
-          <li><Link href="/#contact" className="hover:text-gray-900 transition-colors">Contact</Link></li>
+            <li className="hidden md:block"><Link href="/#projects" className="hover:text-gray-900 transition-colors">Projects</Link></li>
+            <li className="block"><Link href="/#experiences" className="hover:text-gray-900 transition-colors">Experiences</Link></li>
+          <li className="hidden md:block"><Link href="/#contact" className="hover:text-gray-900 transition-colors">Contact</Link></li>
         </ul>
       </nav>
     </header>
